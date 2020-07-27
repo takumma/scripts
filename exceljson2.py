c421ee2401a57250f7356cb00e54df1f1381dc7c
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-07-27 22:37:23 +0900

    update fruit2.json: Add 5 nutrition

--- exceljson2.py
+++ exceljson2.py
@@ -12,7 +12,34 @@ fruits_list = [{
 for i in range(2, 175):
     food_id = sheets.cell(row = i, column = 2).value
     name = sheets.cell(row = i, column = 4).value
+    dietary_fiber = sheets.cell(row = i, column = 21).value
+    potassium = sheets.cell(row = i, column = 24).value
+    iron = sheets.cell(row = i, column = 28).value
+    vitamin_b1 = sheets.cell(row = i, column = 48).value
+    vitamin_c = sheets.cell(row = i, column = 56).value
+    
+    if dietary_fiber == 'Tr':
+        dietary_fiber = 0
+    else:
+        dietary_fiber = float(dietary_fiber)
+    if potassium == 'Tr':
+        potassium = 0
+    else:
+        potassium = float(potassium)
+    if iron == 'Tr':
+        iron = 0
+    else:
+        iron = float(iron)
+    if vitamin_b1 == 'Tr':
+        vitamin_b1 = 0
+    else:
+        vitamin_b1 = float(vitamin_b1)
+    if vitamin_c == 'Tr':
+        vitamin_c = 0
+    else:
+        vitamin_c = float(vitamin_c)
 
+    
     if name.split("　")[0][0] == '(' or name.split("　")[0][0] == '（':
         name = name.split("　")[1]
     else:
@@ -21,7 +48,12 @@ for i in range(2, 175):
     if fruits_list[-1]['name'] != name:
         fruits_list.append({
         "food_id": int(food_id),
-        "name": name
+        "name": name,
+        "dietary_fiber": float(dietary_fiber),
+        "potassium": float(potassium),
+        "iron": float(iron),
+        "vitamin_b1": float(vitamin_b1),
+        "vitamin_c": float(vitamin_c),
         })
     
 # print(fruits_list[-1]['name'] != "name")
