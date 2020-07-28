96a2b74bc710d9eec956c2217e8d0391656be11a
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-07-28 09:16:40 +0900

    fix exceljson.py: a bit of refactoring

--- exceljson2.py
+++ exceljson2.py
@@ -1,22 +1,22 @@
 import openpyxl
 import json
-import codecs
 
-book = openpyxl.load_workbook('C:/Users/taku3/work/scripts/1365344_1-0207r.xlsx')
-sheets = book['07 果実類']
-path_w = 'C:/Users/taku3/work/scripts/fruit2.json'
+load_book = openpyxl.load_workbook('C:/Users/taku3/work/scripts/1365344_1-0207r.xlsx')
+sheet = load_book['07 果実類']
+json_path = 'C:/Users/taku3/work/scripts/fruit2.json'
+
 fruits_list = [{
-        "food_id": 0,
-        "name": ""
-        }]
+    "food_id": 0,
+    "name": ""
+}]
 for i in range(2, 175):
-    food_id = sheets.cell(row = i, column = 2).value
-    name = sheets.cell(row = i, column = 4).value
-    dietary_fiber = sheets.cell(row = i, column = 21).value
-    potassium = sheets.cell(row = i, column = 24).value
-    iron = sheets.cell(row = i, column = 28).value
-    vitamin_b1 = sheets.cell(row = i, column = 48).value
-    vitamin_c = sheets.cell(row = i, column = 56).value
+    food_id = sheet.cell(row = i, column = 2).value
+    name = sheet.cell(row = i, column = 4).value
+    dietary_fiber = sheet.cell(row = i, column = 21).value
+    potassium = sheet.cell(row = i, column = 24).value
+    iron = sheet.cell(row = i, column = 28).value
+    vitamin_b1 = sheet.cell(row = i, column = 48).value
+    vitamin_c = sheet.cell(row = i, column = 56).value
     
     if dietary_fiber == 'Tr':
         dietary_fiber = 0
@@ -39,7 +39,6 @@ for i in range(2, 175):
     else:
         vitamin_c = float(vitamin_c)
 
-    
     if name.split("　")[0][0] == '(' or name.split("　")[0][0] == '（':
         name = name.split("　")[1]
     else:
@@ -47,18 +46,19 @@ for i in range(2, 175):
     
     if fruits_list[-1]['name'] != name:
         fruits_list.append({
-        "food_id": int(food_id),
-        "name": name,
-        "dietary_fiber": float(dietary_fiber),
-        "potassium": float(potassium),
-        "iron": float(iron),
-        "vitamin_b1": float(vitamin_b1),
-        "vitamin_c": float(vitamin_c),
+            "food_id": int(food_id),
+            "name": name,
+            "dietary_fiber": float(dietary_fiber),
+            "potassium": float(potassium),
+            "iron": float(iron),
+            "vitamin_b1": float(vitamin_b1),
+            "vitamin_c": float(vitamin_c),
         })
     
 data_dict = {
     "data": "fruits",
     "fruits": fruits_list
 }
-with open(path_w, mode = 'a', encoding = 'utf-8') as f:
+
+with open(json_path, mode = 'a', encoding = 'utf-8') as f:
     f.write(json.dumps(data_dict, ensure_ascii = False, indent = 4))
